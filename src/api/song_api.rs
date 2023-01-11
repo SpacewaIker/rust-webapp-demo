b4e903f80b6ea34cba98d77b00e45268eac96b8a
Thibaut Baguette <thibaut.baguette@mail.mcgill.ca>
2023-01-11 13:28:37 -0500

    Write basic song api

--- src/api/song_api.rs
+++ src/api/song_api.rs
@@ -1,45 +1,108 @@
-use crate::{model::song::Song, repository::MongoRepo};
-use mongodb::results::{DeleteResult, InsertOneResult};
-use rocket::{http::Status, serde::json::Json, State};
+use crate::entities::{prelude::*, song};
+use rocket::{http::Status, response::status, serde::json::Json, State};
+use sea_orm::{ActiveModelTrait, ActiveValue, DatabaseConnection, EntityTrait};
 
 #[post("/", data = "<song>")]
-pub fn create_song(
-    db: &State<MongoRepo>,
-    song: Json<Song>,
-) -> Result<Json<InsertOneResult>, Status> {
-    db.create_song(song.into_inner()).map(|v| Json(v))
+pub async fn create_song(
+    db: &State<DatabaseConnection>,
+    song: Json<song::Model>,
+) -> Result<(Status, String), (Status, &str)> {
+    let db = db as &DatabaseConnection;
+
+    let album = Album::find_by_id(song.album_id)
+        .one(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error fetching album in DB"))?;
+
+    if song.name.is_empty() || song.length_secs <= 0 || album.is_none() {
+        return Err((Status::BadRequest, "Invalid input"));
+    }
+
+    let new_song = song::ActiveModel {
+        name: ActiveValue::Set(song.name.to_owned()),
+        length_secs: ActiveValue::Set(song.length_secs),
+        album_id: ActiveValue::Set(song.album_id),
+        ..Default::default()
+    };
+
+    let song = Song::insert(new_song)
+        .exec(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error inserting song in DB"))?;
+
+    Ok((Status::Created, song.last_insert_id.to_string()))
 }
 
 #[get("/<id>")]
-pub fn get_song(db: &State<MongoRepo>, id: String) -> Result<Json<Song>, Status> {
-    db.get_song(&id).map(|v| Json(v))
+pub async fn get_song_by_id(
+    db: &State<DatabaseConnection>,
+    id: i32,
+) -> Result<(Status, Json<song::Model>), (Status, &str)> {
+    let db = db as &DatabaseConnection;
+
+    let song = Song::find_by_id(id)
+        .one(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error fetching song in DB"))?;
+
+    Ok((
+        Status::Found,
+        Json(song.ok_or((Status::NotFound, "Song not found"))?),
+    ))
 }
 
 #[put("/<id>", data = "<song>")]
-pub fn update_song(
-    db: &State<MongoRepo>,
-    id: String,
-    song: Json<Song>,
-) -> Result<Json<Song>, Status> {
-    let updated_song = db.update_song(&id, song.into_inner())?;
-    if updated_song.matched_count == 1 {
-        db.get_song(&id).map(|v| Json(v))
-    } else {
-        Err(Status::NotFound)
-    }
+pub async fn update_song(
+    db: &State<DatabaseConnection>,
+    id: i32,
+    song: Json<song::Model>,
+) -> Result<(Status, String), (Status, &str)> {
+    let db = db as &DatabaseConnection;
+
+    let updated_song = song::ActiveModel {
+        id: ActiveValue::Set(id),
+        name: ActiveValue::Set(song.name.to_owned()),
+        length_secs: ActiveValue::Set(song.length_secs),
+        album_id: ActiveValue::Set(song.album_id),
+        ..Default::default()
+    };
+
+    updated_song
+        .update(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error updating song"))?;
+
+    Ok((Status::Accepted, id.to_string()))
 }
 
 #[delete("/<id>")]
-pub fn delete_song(db: &State<MongoRepo>, id: String) -> Result<Json<DeleteResult>, Status> {
-    let delete_result = db.delete_song(&id)?;
-    if delete_result.deleted_count == 1 {
-        Ok(Json(delete_result))
-    } else {
-        Err(Status::NotFound)
+pub async fn delete_song(
+    db: &State<DatabaseConnection>,
+    id: i32,
+) -> Result<(Status, String), (Status, &str)> {
+    let db = db as &DatabaseConnection;
+
+    let res = Song::delete_by_id(id)
+        .exec(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error deleting song"))?;
+
+    match res.rows_affected {
+        1 => Ok((Status::Ok, id.to_string())),
+        _ => Err((Status::NotFound, "Song not found")),
     }
 }
 
 #[get("/all")]
-pub fn get_all_songs(db: &State<MongoRepo>) -> Result<Json<Vec<Song>>, Status> {
-    db.get_all_songs().map(|v| Json(v))
+pub async fn get_all_songs(
+    db: &State<DatabaseConnection>,
+) -> Result<(Status, Json<Vec<song::Model>>), (Status, &str)> {
+    let db = db as &DatabaseConnection;
+
+    let songs = Song::find()
+        .all(db)
+        .await
+        .map_err(|_| (Status::InternalServerError, "Error fetching songs in DB"))?;
+
+    Ok((Status::Ok, Json(songs)))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,45 @@
 mod api;
-mod model;
-mod repository;
+mod migrator;
+mod entities;
 
 #[macro_use]
 extern crate rocket;
 
-use repository::MongoRepo;
+use dotenv::dotenv;
+use sea_orm::{DatabaseConnection, Database};
+use sea_orm_migration::prelude::{SchemaManager, MigratorTrait};
+use std::env;
+
+pub async fn set_up_db() -> DatabaseConnection {
+    dotenv().ok();
+    let url = match env::var("POSTGRES_URL") {
+        Ok(v) => v.to_string(),
+        Err(_) => format!("Error loading env variable"),
+    };
+    Database::connect(url).await.expect("Error connecting to database")
+}
 
 #[launch]
-fn rocket() -> _ {
-    let db = MongoRepo::init();
+async fn rocket() -> _ {
+    let db = set_up_db().await;
+
+    let schema_manager = SchemaManager::new(&db);
+    migrator::Migrator::refresh(&db).await.unwrap();
+    assert!(schema_manager.has_table("song").await.unwrap());
+    assert!(schema_manager.has_table("album").await.unwrap());
+    assert!(schema_manager.has_table("artist").await.unwrap());
+    assert!(schema_manager.has_table("album_artist_mtm").await.unwrap());
+
     rocket::build()
+        .manage(db)
         .mount(
             "/song",
             routes![
                 api::song_api::create_song,
-                api::song_api::get_song,
+                api::song_api::get_song_by_id,
                 api::song_api::update_song,
                 api::song_api::delete_song,
                 api::song_api::get_all_songs,
             ],
         )
-        .manage(db)
 }
