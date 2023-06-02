833cf248034e37786d66b3ac081acabe380b9b3b
Thibaut Baguette <thibaut.baguette@mail.mcgill.ca>
2023-06-02 10:56:34 -0400

    Change homepage to display README
    Change the app's homepage to display the project's README.md which is fetched from the repository.

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,11 +1,34 @@
+use std::ops::Deref;
+
+use gloo_net::http::Request;
+use markdown::to_html;
 use yew::prelude::*;
 
 #[function_component(HomePage)]
 pub fn home() -> Html {
-    html! {
-        <div>
-            <h1>{"Home"}</h1>
-            <p>{"Welcome to the home page!"}</p>
-        </div>
+    let content = use_state(|| Html::default());
+
+    {
+        let content = content.clone();
+        use_effect_with_deps(
+            move |_| {
+                wasm_bindgen_futures::spawn_local(async move {
+                    let readme = Request::get("https://raw.githubusercontent.com/SpacewaIker/rust-webapp-demo/main/README.md")
+                        .send()
+                        .await
+                        .expect("Failed to fetch README.md")
+                        .text()
+                        .await
+                        .expect("Failed to read README.md");
+
+                    let html_string = to_html(&readme);
+                    let html = Html::from_html_unchecked(html_string.into());
+                    content.set(html);
+                })
+            },
+            (),
+        )
     }
+
+    content.deref().clone()
 }
